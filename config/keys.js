2f885c181216d9b9c90a2b6bf50a4350de17231c
shubhkhanna <khannashubh2001@gmail.com>
2022-12-27 17:38:20 +0530

    ft[server]: added keys file

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config();
+module.exports = {
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
