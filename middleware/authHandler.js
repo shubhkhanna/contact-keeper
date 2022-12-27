120a7c8fce2419f55f560e74befe28166d96c92f
shubhkhanna <khannashubh2001@gmail.com>
2022-12-27 21:11:44 +0530

    ft[server]: added auth handler for private routes

---
+++ middleware/authHandler.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const { JWT_SECRET } = require("../config/keys");
+const User = require("../models/userModel");
+
+const authHandler = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      //   decoding the token
+      const decoded = jwt.verify(token, JWT_SECRET);
+
+      req.user = await User.findById(decoded.userId).select("-password");
+
+      next();
+    } catch (error) {
+      // if token is expired
+      if (error.name === "TokenExpiredError") {
+        res.status(401);
+        throw new Error("Token Expired!");
+      }
+
+      // if token is invalid
+      if (error.name === "JsonWebTokenError") {
+        res.status(401);
+        throw new Error("Invalid Token!");
+      }
+    }
+  }
+
+  // If no token
+  if (!token) {
+    res.status(401);
+    throw new Error("Unauthorized Access, No Token Provided!");
+  }
+});
+
+module.exports = authHandler;
