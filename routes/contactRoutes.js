54d2c73e6e69289a19c007bb451c2d3dd3f1b73c
shubhkhanna <khannashubh2001@gmail.com>
2022-12-29 22:41:40 +0530

    ft[server]: contact routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,27 @@
 const express = require("express");
-const { getAllContacts } = require("../controllers/contactController");
+const {
+  getAllContacts,
+  createContact,
+  updateContact,
+  deleteContact,
+  getAllDeletedContacts,
+} = require("../controllers/contactController");
 const authHandler = require("../middleware/authHandler");
+const {
+  validateCreateContact,
+  validate,
+  validateUpdateContact,
+} = require("../utils/contactValidation");
 const router = express.Router();
 
-router.route("/").get(authHandler, getAllContacts);
+router
+  .route("/")
+  .get(authHandler, getAllContacts)
+  .post(authHandler, validateCreateContact, validate, createContact);
+router
+  .route("/:id")
+  .put(authHandler, validateUpdateContact, validate, updateContact)
+  .delete(authHandler, deleteContact);
+router.route("/deleted").get(authHandler, getAllDeletedContacts);
 
 module.exports = router;
