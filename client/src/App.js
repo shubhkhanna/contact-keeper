3b79ac4dcf93a358eacf6b57500b3fd643063d3d
shubhkhanna <khannashubh2001@gmail.com>
2023-01-16 02:18:43 +0530

    ft[client]: root components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
+import PrivateRoute from "./components/PrivateRoute";
+import { AppRoutes } from "./utils/routes";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Private Routes */}
+        <Route element={<PrivateRoute />}>
+          <Route path={AppRoutes.home} element={<HomePage />} />
+        </Route>
+
+        {/* Public Routes */}
+        <Route path={AppRoutes.signin} element={<SigninPage />} />
+        <Route path={AppRoutes.signup} element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { Toaster } from "react-hot-toast";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Toaster
+        gutter={5}
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{ duration: 3000 }}
+      />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
