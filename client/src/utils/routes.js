eccdfc0032bb3cb9bfc8fe9a52833e6c6363a04d
shubhkhanna <khannashubh2001@gmail.com>
2023-01-15 21:00:49 +0530

    ft[client]: added routes file

---
+++ client/src/utils/routes.js
@@ -0,0 +1,10 @@
+export const AppRoutes = {
+  home: "/",
+  signin: "/signin",
+  signup: "/signup",
+};
+
+export const ServerRoutes = {
+  signup: "/v1/user/signup",
+  signin: "/v1/user/signin",
+};
