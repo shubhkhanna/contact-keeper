1a395516131ecd29bc0f0b8f6d8b4b2d20c0e4e1
shubhkhanna <khannashubh2001@gmail.com>
2023-01-16 02:15:15 +0530

    ft[client]: added user slices

---
+++ client/src/redux/services/userServices.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../utils/axios";
+import { ServerRoutes } from "../../utils/routes";
+
+// signin user thunk
+export const signinUser = createAsyncThunk(
+  "user/signin",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const { data } = await axios.post(ServerRoutes.signin, {
+        email,
+        password,
+      });
+
+      if (data.success) return data;
+    } catch (err) {
+      const { data } = err.response;
+      return thunkAPI.rejectWithValue(data);
+    }
+  }
+);
+
+// signup user thunk
+export const signupUser = createAsyncThunk(
+  "user/signup",
+  async ({ name, email, password }, thunkAPI) => {
+    try {
+      const { data } = await axios.post(ServerRoutes.signup, {
+        name,
+        email,
+        password,
+      });
+
+      if (data.success) return data;
+    } catch (err) {
+      const { data } = err.response;
+      return thunkAPI.rejectWithValue(data);
+    }
+  }
+);
