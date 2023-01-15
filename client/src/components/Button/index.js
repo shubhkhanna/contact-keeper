015851b502f8eac1d8a923c9fdc95a95a8994835
shubhkhanna <khannashubh2001@gmail.com>
2023-01-15 20:24:05 +0530

    chore[client]: updated button props

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,16 +1,19 @@
 import ClassNames from "classnames";
+import Spinner from "../Spinner";
 
-const Button = ({ type, onClick, buttonText, styles = "" }) => {
+const Button = ({ type, onClick, buttonText, disabled, styles = "" }) => {
   return (
     <button
+      disabled={disabled}
       type={type}
       onClick={onClick}
       className={ClassNames(
-        "bg-blue-700 hover:bg-blue-800 text-white font-medium text-sm px-5 py-3 text-center rounded-lg mt-4 w-full",
+        "bg-blue-700 text-white font-medium text-sm px-5 py-3 text-center rounded-lg mt-4 w-full",
+        disabled ? "bg-gray-400 cursor-not-allowed" : "hover:bg-blue-800",
         styles
       )}
     >
-      {buttonText}
+      {disabled ? <Spinner /> : buttonText}
     </button>
   );
 };
