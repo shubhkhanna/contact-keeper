0e5b5dadaa4642e38a046f9246ccc8913d0a186a
shubhkhanna <khannashubh2001@gmail.com>
2023-01-19 19:40:20 +0530

    ft[client]:  added proxy server

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
+  proxy: { host: "localhost", port: 3000 },
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
